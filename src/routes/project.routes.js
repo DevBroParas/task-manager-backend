b0d22c71f09dfc6985535d402bf11143ed79dd1a
DevBroParas <parasprofessionaly@gmail.com>
2025-04-30 15:28:55 +0530

    added auth middleware

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { CreateProject, GetProjects } from "../controllers/project.controller";
+import { protect } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.post("/projects", CreateProject);
+router.post("/projects", protect, CreateProject);
 
-router.get("/projects", GetProjects);
+router.get("/projects", protect, GetProjects);
 
 export default router;
